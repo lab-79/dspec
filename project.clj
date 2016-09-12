5aeea7ed90504722850254c5a89858db806060c0
Brian Noguchi <brian.noguchi@gmail.com>
2016-09-12 15:13:39 -0700

    Bump version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lab79/dspec "0.2.0"
+(defproject lab79/dspec "0.3.0"
   :description "Stronger semantics on top of Datomic, with clojure.spec goodies."
   :url "https://github.com/lab-79/dspec"
   :license {:name "Eclipse Public License"
