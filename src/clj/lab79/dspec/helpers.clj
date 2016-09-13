4a13595e6dc5ef1bdcd83eb6d09c3b1bcbc91776
Brian Noguchi <brian.noguchi@gmail.com>
2016-09-12 18:46:49 -0700

    Fixes for helper fns

--- src/clj/lab79/dspec/helpers.clj
+++ src/clj/lab79/dspec/helpers.clj
@@ -1,26 +1,27 @@
 (ns lab79.dspec.helpers
   (:require [lab79.dspec :refer [semantic-ast->datomic-schemas
                                  semantic-spec-coll->semantic-ast
-                                 register-specs-for-ast!]]))
+                                 register-specs-for-ast!
+                                 register-specs-for-ast-with-custom-generators!]]))
 
 (defn specs->datomic
-  [specs]
+  [specs tempid-factory]
   (-> specs
       semantic-spec-coll->semantic-ast
-      semantic-ast->datomic-schemas))
+      (semantic-ast->datomic-schemas tempid-factory)))
 
 (defn create-clojure-specs!
-  [specs]
+  [specs tempid-factory db-id?]
   (-> specs
       semantic-spec-coll->semantic-ast
-      register-specs-for-ast!))
+      register-specs-for-ast! tempid-factory db-id?))
 
 (defn create-generative-clojure-specs!
   "Registers all clojure.specs with custom generators. Specs are auto-created
   based off of our interface definitions in resources/data/schemas/ and
   custom generators for these specs and their fields that override the
   defaults are located in src/ccm_om_next/db/gen/all"
-  [specs generators]
+  [specs generators tempid-factory db-id?]
   (-> specs
       semantic-spec-coll->semantic-ast
-      (register-specs-for-ast! generators)))
+      (register-specs-for-ast-with-custom-generators! generators tempid-factory db-id?)))
