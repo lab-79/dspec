c14162bd48501ca1c248c6fdcf2996ff99afbe60
Brian Noguchi <brian@lab79.org>
2016-11-18 14:39:12 -0800

    Add docstring to placeholder lab79.dspec.macros ns

--- src/cljc/lab79/dspec/macros.clj
+++ src/cljc/lab79/dspec/macros.clj
@@ -1,4 +1,10 @@
 (ns lab79.dspec.macros
+  "For cljs, we need to convert some of our calls that are function calls in clojure
+  to macro calls in clojurescript because clojure.spec definitions are macros and
+  the way that clojure dspec works is that it constructs those clojure.spec definitions
+  at runtime. Since clojurescript only supports macros via AOT compilation, we need to
+  convert some of the runtime functions of clojure dspec into macros that are expanded
+  during clojurescript's AOT compilation."
   (:require [clojure.spec :as s]))
 
 ; ast&interface->ast-fields
